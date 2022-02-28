e0ef37afffad0c27e2abafe21d4a2c7275010adf
JacquesLJT <landonthib@gmail.com>
2022-02-27 23:17:59 -0500

    More work on signup

--- Development/cswap/src/components/App.js
+++ Development/cswap/src/components/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import './App.css';
 import Signup from './Signup';
+import { Container } from 'react-bootstrap';
+import { AuthProvider } from '../contexts/AuthContext';
+import './App.css';
 
 function App() {
   return (
-    <Signup />
+    <AuthProvider>
+      <Container className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: '100vh' }}
+      >
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <Signup />
+        </div>
+      </Container>
+    </AuthProvider>
   );
 }
 
--- Development/cswap/src/firebase.js
+++ Development/cswap/src/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
-import firebase from "firebase/app"
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
 import "firebase/auth"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,6 +19,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 export const auth = app.auth();
 export default app 
