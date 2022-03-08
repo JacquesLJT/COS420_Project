fa8508eaf22b37ee52bfe1668f7beaf5b8ad7e92
anthonycaccese1 <anthony.caccese@maine.edu>
2022-03-08 14:09:36 -0500

    Update Home.js

--- Development/cswap/src/components/Home.js
+++ Development/cswap/src/components/Home.js
@@ -1,5 +1,19 @@
+import { 
+  Center,
+  Stack,
+  Heading, 
+  Text, 
+  Button, 
+  Checkbox, 
+  Input, 
+  InputLeftAddon, 
+  InputGroup,
+  Alert,
+  Image
+} from '@chakra-ui/react';
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Formik, Form } from 'formik';
+// import { Button } from 'react-bootstrap';
 import { useNavigate } from 'react-router';
 import { useUserAuth } from '../context/UserAuthContext';
 
@@ -15,17 +29,19 @@ const Home = () => {
     }
   };
   return (
-    <>
-      <div className="p-4 box mt-3 text-center">
-        Hello Welcome <br />
-        {user && user.email}
-      </div>
-      <div className="d-grid gap-2">
-        <Button variant="primary" onClick={handleLogout}>
-          Log out
-        </Button>
-      </div>
-    </>
+    <Center h="100vh" bg="#013220">
+      <Stack boxShadow="md" bg="whiteAlpha.900" p='20' rounded="md">
+        <div className="p-4 box mt-3 text-center">
+          Hello Welcome <br />
+          {user && user.email}
+        </div>
+        <div className="d-grid gap-2">
+          <Button colorScheme="green" onClick={handleLogout}>
+            Log out
+          </Button>
+        </div>
+      </Stack>
+    </Center>
   );
 };
 
