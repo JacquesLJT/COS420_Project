2c87f1371835cfe0c7316f1f95ea0271465d0560
anthonycaccese1 <anthony.caccese@maine.edu>
2022-04-08 14:34:50 -0400

    Added Image picker

--- Development/cswap/src/components/CreateListing.js
+++ Development/cswap/src/components/CreateListing.js
@@ -23,6 +23,7 @@ import { Switch, Routes, Route, Navigate, Link,} from 'react-router-dom';
 import { useNavigate } from 'react-router';
 import { db } from '../firebase';
 import { addDoc, collection } from 'firebase/firestore';
+import ReactImageUploading from 'react-images-uploading';
 
 const createTextBookListing = async () => {
     console.log("Yoyo")
@@ -88,6 +89,58 @@ const LoadTextBookForm = async () => {
     navigate("/textbook");
 };
 
+function ImageUploader(){
+    const [images, setImages] = React.useState([]);
+    const maxNumber = 69;
+    const onChange = (imageList, addUpdateIndex) => {
+      // data for submit
+      console.log(imageList, addUpdateIndex);
+      setImages(imageList);
+    };
+    return (
+    <div>
+        <ReactImageUploading
+            multiple
+            value={images}
+            onChange={onChange}
+            maxNumber={maxNumber}
+            dataURLKey="data_url"
+        >
+            {({
+            imageList,
+            onImageUpload,
+            onImageRemoveAll,
+            onImageUpdate,
+            onImageRemove,
+            isDragging,
+            dragProps,
+            }) => (
+            // write your building UI
+            <div className="upload__image-wrapper">
+                <button
+                style={isDragging ? { color: 'red' } : undefined}
+                onClick={onImageUpload}
+                {...dragProps}
+                >
+                Click or Drop here
+                </button>
+                &nbsp;
+                <button onClick={onImageRemoveAll}>Remove all images</button>
+                {imageList.map((image, index) => (
+                <div key={index} className="image-item">
+                    <img src={image['data_url']} alt="" width="100" />
+                    <div className="image-item__btn-wrapper">
+                    <button onClick={() => onImageUpdate(index)}>Update</button>
+                    <button onClick={() => onImageRemove(index)}>Remove</button>
+                    </div>
+                </div>
+                ))}
+            </div>
+            )}
+        </ReactImageUploading>
+        </div>
+    );
+}
 function TextBookForm() {
     return (
         <Stack boxShadow="md" bg="whiteAlpha.900" p='10' rounded="md" w="50%" >  
@@ -123,7 +176,9 @@ function TextBookForm() {
                             cols={40}
                             >
                         </Textarea>
+                    <ImageUploader></ImageUploader>
                     </Center>
+
                 </VStack>
                 <Button colorScheme={"green"}><Link to="/home">Create</Link></Button>
             </HStack>
